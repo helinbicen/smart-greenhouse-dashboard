e93eed82db7981be6bd625ff494181e3b8e3d5be
Helin Biçen <helinbicen@outlook.com>
2025-05-02 23:09:05 +0300

    Add basic project structure

---
+++ src/app/cotton/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Cotton</div>
+  )
+}
+
+export default page
---
+++ src/components/SensorCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SensorCard = () => {
+  return (
+    <div>SensorCard</div>
+  )
+}
+
+export default SensorCard
