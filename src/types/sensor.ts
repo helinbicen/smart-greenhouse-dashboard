8ea14de53344d06a269b13b93f732c50c0495af9
Helin Biçen <helinbicen@outlook.com>
2025-05-02 23:23:54 +0300

    Add types & mock data

---
+++ src/types/sensor.ts
@@ -0,0 +1,24 @@
+export const TemperatureCategory = {
+  LOW: "low",
+  MEDIUM: "medium",
+  HIGH: "high",
+} as const;
+
+export const MoistureCategory = {
+  DRY: "dry",
+  OPTIMAL: "optimal",
+  WET: "wet",
+} as const;
+
+export type SensorData = {
+  moisture: number;
+  temperature: number;
+  pumperOn: boolean;
+  heaterOn: boolean;
+  coolerOn: boolean;
+  temperatureCategory: (typeof TemperatureCategory)[keyof typeof TemperatureCategory];
+  moistureCategory: (typeof MoistureCategory)[keyof typeof MoistureCategory];
+  heatingDemand: number;
+  coolingDemand: number;
+  moistureTemperatureRatio: number;
+};
